72f72f774a813b0b50a9910276a40fbc769af88f
caRRo-prof <ruizrobledo@gmail.com>
2025-08-07 07:31:25 -0600

    Crea proyecto en NetBeans

---
+++ web/app.js
@@ -0,0 +1,29 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica 
+// de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/**
+ * Los usuarios escribirán el nombre de un amigo en un campo de texto y lo 
+ * agregarán a una lista visible al hacer clic en "Adicionar".
+ */
+function agregarNombres(parameters) {
+    
+}
+/**
+ Si el campo de texto está vacío, el programa mostrará una alerta 
+ pidiendo un nombre válido.
+ */
+function validarEntrada(parameters) {
+    
+}
+/**
+ *Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+ */
+function visualizarLista(parameters) {
+    
+}
+/**
+ * Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente 
+ * un nombre de la lista y se mostrará en la página.
+ */
+function sortearAleatorio(parameters) {
+    
+}
